2c6f89022e623cd601831c579141e48e01dc4312
Ben S. Anderson <ben.anderson@assurant.com>
2019-02-28 07:27:11 -0500

    added getting opcode from incoming request

--- TFTPServer.py
+++ TFTPServer.py
@@ -1,8 +1,16 @@
 import socketserver
+from tftp import proto
 
 
 class TFTPServer(socketserver.DatagramRequestHandler):
     def handle(self):
-        data = self.request[0].strip()
+        request = self.request[0].strip()
+
+        # Get the packet opcode and dispatch
+        opcode = proto.TFTPHelper.get_opcode(request)
+
         print('{} wrote:'.format(self.client_address[0]))
-        print(data)
+        print(request)
+        print('opcode is {}'.format(opcode))
+
+
