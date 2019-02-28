c896b0593a7bc0af062e0506d8c4deae5b71afa5
Ben S. Anderson <ben.anderson@assurant.com>
2019-02-28 18:09:48 -0500

    using original server and debugging

--- tftp.py
+++ tftp.py
@@ -1,10 +1,11 @@
-from TFTPServer import TFTPServer
-import socketserver
+from tftp.Server import Server
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
-print('creating server')
-server = socketserver.UDPServer(('localhost', 4234), TFTPServer)
-print('serving')
+
+iface = 'lo0'
+root = '/Users/benanderson/tftp'
+port = 4234
+
+server = Server(iface, root, port)
 server.serve_forever()
-print('closing')
