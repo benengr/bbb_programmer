01ca5cbb27cd65aeb53143a985045bad320b3665
Ben S. Anderson <ben.anderson@assurant.com>
2019-02-28 20:37:43 -0500

    set log level

--- bootp.py
+++ bootp.py
@@ -1,4 +1,7 @@
 from bootp.BOOTPServer import BOOTPServer
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 server = BOOTPServer('usb0', None, '192.168.4.1', '192.168.4.1')
 server.serve_forever()
