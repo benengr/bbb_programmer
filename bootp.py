909fa8cb238b64316501a5b94cad9068e66b9763
Ben S. Anderson <ben.anderson@assurant.com>
2019-02-28 20:42:42 -0500

    added retry logic in case network goes down

--- bootp.py
+++ bootp.py
@@ -2,6 +2,17 @@ from bootp.BOOTPServer import BOOTPServer
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
-
+log = logging.getLogger('main')
 server = BOOTPServer('usb0', None, '192.168.4.1', '192.168.4.1')
-server.serve_forever()
+
+done = False
+while not done:
+    try:
+        log.info("Starting Server")
+        server.serve_forever()
+    except KeyboardInterrupt:
+        log.info("Keyboard interrupt, shutting down")
+        done = True
+    except Exception as ex:
+        log.exception(ex)
+        log.info("restarting server")
