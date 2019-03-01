3d44f662347edf1bc9442306f74a2a4f56cef386
Ben S. Anderson <ben.anderson@assurant.com>
2019-02-28 22:17:44 -0500

    made threads for bootp and tftp

--- bootp.py
+++ bootp.py
@@ -1,18 +1,22 @@
 from bootp.BOOTPServer import BOOTPServer
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
-log = logging.getLogger('main')
-server = BOOTPServer('usb0', None, '192.168.4.1', '192.168.4.1')
 
-done = False
-while not done:
-    try:
-        log.info("Starting Server")
-        server.serve_forever()
-    except KeyboardInterrupt:
-        log.info("Keyboard interrupt, shutting down")
-        done = True
-    except Exception as ex:
-        log.exception(ex)
-        log.info("restarting server")
+def run_server():
+    log = logging.getLogger('bootpthread')
+    server = BOOTPServer('usb0', None, '192.168.4.1', '192.168.4.1')
+    done = False
+    while not done:
+        try:
+            log.info("Starting server")
+            server.serve_forever()
+        except KeyboardInterrupt:
+            done = True
+        except Exception as ex:
+            log.info("Restarting Server")
+            pass
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    run_server()
--- main.py
+++ main.py
@@ -1,2 +1,37 @@
-import fbtftp
+import threading
+import logging
+from bootp.BOOTPServer import BOOTPServer
+from tftp.Server import Server
 
+
+def start_bootp():
+    log = logging.getLogger('bootpthread')
+    server = BOOTPServer('usb0', None, '192.168.4.1', '192.168.4.1')
+    done = False
+    while not done:
+        try:
+            log.info("Starting server")
+            server.serve_forever()
+        except KeyboardInterrupt:
+            done = True
+        except Exception as ex:
+            log.info("Restarting Server")
+            pass
+
+
+def start_tftp(iface, root, port):
+    server = Server(iface, root, port)
+    server.serve_forever()
+
+
+if __name__ == "__main__":
+    bootp_thread = threading.Thread(target=start_bootp)
+    tftp_thread = threading.Thread(target=tftp_thread)
+    logging.info("Starting bootp thread")
+    bootp_thread.run()
+    logging.info("Starting tftp thread")
+    tftp_thread.run()
+    logging.info("Joining threads")
+    bootp_thread.join()
+    tftp_thread.join()
+    logging.info("Exiting")
