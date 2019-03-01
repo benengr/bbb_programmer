4c0ca223d50cd1db3a2e3a967543cc3b3acbc123
Ben S. Anderson <ben.anderson@assurant.com>
2019-03-01 09:46:49 -0500

    polling to see if interface is available

--- main.py
+++ main.py
@@ -2,49 +2,57 @@ import threading
 import logging
 from bootp.BOOTPServer import BOOTPServer
 from tftp.Server import Server
+import netifaces
+import time
+
+IFACE = 'usb0'
+TFTP_ROOT = '/var/tftproot'
+TFTP_PORT = 69
 
 
-def start_bootp():
-    log = logging.getLogger('bootpthread')
-    server = BOOTPServer('usb0', None, '192.168.4.1', '192.168.4.1')
-    done = False
-    while not done:
-        try:
-            log.info("Starting server")
-            server.serve_forever()
-        except KeyboardInterrupt:
-            done = True
-        except Exception as ex:
-            log.info("Restarting Server")
-            pass
+def wait_for_interface(iface, poll_time, logger):
+    logger.info("Waiting for %s to become available", iface)
+    while True:
+        logger.debug("Checking if %s exists", iface)
+        if iface in netifaces.interfaces():
+            try:
+                logger.debug("%s exists, trying to find IP address", iface)
+                return netifaces.ifaddresses(iface)
+            except ValueError as error:
+                logger.error("Could not get an address for %s", iface)
+                logger.exception(error)
+                pass
+        time.sleep(poll_time)
 
 
-def start_tftp():
-    iface = '0.0.0.0'
-    root = '/var/tftproot'
-    port = 69
-    log = logging.getLogger('tftpthread')
-    done = False
-    while not done:
-        try:
-            log.info("Starting server on %s:%d with root %s" % (iface, port, root))
-            server = Server(iface, root, port)
-            server.serve_forever()
-        except Exception as ex:
-            log.exception(ex)
-        pass
-    pass
+def start_bootp(ip):
+    server = BOOTPServer(IFACE, None, ip, ip)
+    server.serve_forever()
+
+
+def start_tftp(ip):
+    server = Server(ip, TFTP_ROOT, TFTP_PORT)
+    server.serve_forever()
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
-    bootp_thread = threading.Thread(target=start_bootp)
-    tftp_thread = threading.Thread(target=start_tftp)
-    logging.info("Starting bootp thread")
-    bootp_thread.start()
-    logging.info("Starting tftp thread")
-    tftp_thread.start()
-    logging.info("Joining threads")
-    bootp_thread.join()
-    tftp_thread.join()
-    logging.info("Exiting")
+    log = logging.getLogger('main')
+    log.info("System Started")
+    try:
+        while True:
+            address = wait_for_interface(IFACE, 1, log)
+            logging.info("found interface with ip: %s", address)
+            bootp_thread = threading.Thread(target=start_bootp)
+            tftp_thread = threading.Thread(target=start_tftp, args=address)
+            log.info("Starting bootp thread")
+            bootp_thread.start()
+            log.info("Starting tftp thread")
+            tftp_thread.start()
+            log.info("Waiting for threads to complete")
+            bootp_thread.join()
+            tftp_thread.join()
+    except Exception as ex:
+        log.error("unhandled exception occurred")
+        log.exception(ex)
+    log.info("Exiting")
