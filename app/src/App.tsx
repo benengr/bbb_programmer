1357314ed696bfd33cd22eab1923cf9d393b7419
Ben S. Anderson <ben.anderson@assurant.com>
2019-03-15 16:10:27 -0400

    refactored upload to it's own component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,66 +1,16 @@
 import React, { Component, ChangeEvent } from 'react';
 import axios from 'axios';
 import './App.css';
+import FileUpload from './components/FileUpload';
 
-interface Props {}
-interface State {
-  selectedFile: File | null,
-  loaded: number;
-}
 
-class App extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      selectedFile: null,
-      loaded: 0,
-    };
 
-    this.handleUpload = this.handleUpload.bind(this);
-    this.handleSelectedFile = this.handleSelectedFile.bind(this);
-  }
-
-  handleSelectedFile(event: ChangeEvent<HTMLInputElement>) {
-    if(event.target.files === null) {
-      return;
-    }
-    this.setState({selectedFile: event.target.files[0]});
-  }
-
-  handleUpload(ignored: any) {
-    if(this.state.selectedFile === null) {
-      return;
-    }
-    const data = new FormData();
-    data.append('firmware', this.state.selectedFile);
-    
-    axios.post('http://localhost:8080/api/v1/upload', data, {
-      headers: {
-        'content-type': 'multipart/form-data'
-      },
-      onUploadProgress: ProgressEvent => {
-        this.setState({
-          loaded: ProgressEvent.loaded / ProgressEvent.total * 100,
-        })
-      },
-    }).then(res => {
-      console.log(res.statusText);
-    }).catch((error) => {
-      alert(error);
-    })
-  }
-
+class App extends Component {
+  
   render() {
     return (
       <div className="App">
-        <h1>Select a File to Upload</h1>
-        <input type='file' name='firmware' onChange={this.handleSelectedFile} />
-        <button type='submit' onClick={this.handleUpload}>Upload</button>
-        
-        
-        <div>
-          Uploaded: {this.state.loaded}
-        </div>
+        <FileUpload />
       </div>
     );
   }
