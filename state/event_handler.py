73bc2def63f4b8668909cd4c405434b64f26ff84
Ben Anderson <benengr@gmail.com>
2019-03-17 20:02:24 -0400

    updated handling of idle state

--- state/event_handler.py
+++ state/event_handler.py
@@ -1,4 +1,3 @@
-import asyncio
 import state.leds as leds
 
 
@@ -6,8 +5,9 @@ STATE_IDLE = 0
 STATE_READY = 1
 STATE_STARTED = 2
 STATE_ERROR = 1000
-IDLE_TIMEOUT = 5
-
+READY_LIMIT = 4 * 30 # 30 Seconds
+STARTED_LIMIT = 1 # .25 Seconds
+ERROR_LIMIT = 4 * 30 # 30 Seconds
 
 class EventHandler:
     def __init__(self):
@@ -18,22 +18,34 @@ class EventHandler:
     def booted_system_connected(self):
         self.current = STATE_STARTED
         self.set_led_for_state()
+        self.idle_count = 0
 
     def unknown_system_connected(self):
         if self.current == STATE_ERROR or self.current == STATE_IDLE:
             self.current = STATE_READY
             self.set_led_for_state()
+            self.idle_count = 0
 
     def error(self):
         self.current = STATE_ERROR
         self.set_led_for_state()
 
     def no_connection(self):
-        if self.idle_count < IDLE_TIMEOUT:
+        if self.current == STATE_READY:
             self.idle_count += 1
-        if self.idle_count >= IDLE_TIMEOUT:
-            self.current = STATE_IDLE
-            self.set_led_for_state()
+            if self.idle_count > READY_LIMIT:
+                self.current = STATE_IDLE
+                self.set_led_for_state()
+        if self.current == STATE_STARTED:
+            self.idle_count += 1
+            if self.idle_count > STARTED_LIMIT:
+                self.current = STATE_IDLE
+                self.set_led_for_state()
+        if self.current == STATE_ERROR:
+            self.idle_count += 1
+            if self.idle_count > ERROR_LIMIT:
+                self.current = STATE_IDLE
+                self.set_led_for_state()
 
     def set_led_for_state(self):
         state = self.current
